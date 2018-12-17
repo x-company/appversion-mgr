016e9a7a437b4a41ab51a40326bc425567fb30d6
Roland Breitschaft <roland.breitschaft@x-company.de>
2018-12-17 18:19:55 +0100

    Add other Project Files

--- bin/appvmgr.ts
+++ bin/appvmgr.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 /**
  * Copyright (c) 2018 IT Solutions Roland Breitschaft <info@x-company.de>
  *
@@ -8,33 +10,98 @@
  * @Email: roland.breitschaft@x-company.de
  * @Create At: 2018-12-15 00:53:57
  * @Last Modified By: Roland Breitschaft
- * @Last Modified At: 2018-12-15 14:24:25
+ * @Last Modified At: 2018-12-17 18:11:35
  * @Description: This is description.
  */
 
-import * as program from 'commander';
-import * as handler from '../lib';
+import { Command } from 'commander';
+import { getVersion } from '../lib/';
 import { UpdateCommand } from '../lib/UpdateCommand';
+import { SetCommand } from '../lib/SetCommand';
+import { Helper } from '../lib/Helper';
+import { BadgeHelper } from '../lib/BadgeHelper';
 
-
-
+const program = new Command();
 
 program
-    .version('0.1.0')
-    .description('');
+    .version(getVersion(), '-v, --version')
+    .description('AppVersion Manager is a CLI tool whose purpose is to provide a unique manager of the version of you application.');
+
+program
+    .command('init')
+    .description('Generates the appversion.json file.')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
+    .action((options) => {
+
+        const directory = options.directory || undefined;
+
+        const helper = new Helper(directory);
+        const emptyAppVersion = helper.createEmptyAppVersion();
+        helper.writeJson(emptyAppVersion);
+    });
 
 program
     .command('update <action>')
-    .option('-d, --directory', 'Specifies the directory where appvmgr should look for the appversion.json')
     .description('Updates the <action> that can be (major|breaking)|(minor|feature)|(patch|fix)|build|commit')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
     .action((action, options) => {
 
-        const directory = options.directory || __dirname;
+        const directory: string = options.directory || __dirname;
         action = action || 'commit';
 
         const command = new UpdateCommand(directory);
         command.update(action);
     });
+
+program
+    .command('set-version <version>')
+    .description('Sets a specific version number, the <version> must be x.y.z')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
+    .action((version, options) => {
+
+        const directory: string = options.directory || __dirname;
+
+        const command = new SetCommand(directory);
+        command.setVersion(version);
+    });
+
+program
+    .command('set-status <status>')
+    .description('Sets a specific status, the <status> stage can be stable|rc|beta|alpha and the number must be a number')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
+    .action((status, options) => {
+
+        const directory: string = options.directory || __dirname;
+
+        const command = new SetCommand(directory);
+        command.setStatus(status);
+    });
+
+program
+    .command('generate-badge <param>')
+    .description('Generates the .md code of a shield badge with the version of your application, <param> can be version|status')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
+    .action((param, options) => {
+
+        const directory: string = options.directory || __dirname;
+
+        const command = new BadgeHelper(directory);
+        command.createBadge(param);
+    });
+
+program
+    .command('add-git-tag')
+    .description('Adds a tag with the version number to the git repo.')
+    .option('-d, --directory <directory>', 'Specifies the directory where appvmgr should create the appversion.json')
+    .action((options) => {
+
+        const directory: string = options.directory || __dirname;
+
+        const command = new Helper(directory);
+        command.addGitTag();
+    });
+
+
 
 if (!process.argv.slice(2).length) {
     program.outputHelp();
