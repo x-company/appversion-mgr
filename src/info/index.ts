b7b9d177b0669f14c2b7a4033a6b64c1f5d5966f
Roland Breitschaft <roland.breitschaft@x-company.de>
2018-12-19 00:23:16 +0100

    Remove methods from Barrel

--- src/info/index.ts
+++ src/info/index.ts
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (c) 2018 IT Solutions Roland Breitschaft <info@x-company.de>
  *
@@ -9,30 +10,8 @@
  * @Email: roland.breitschaft@x-company.de
  * @Create At: 2018-12-18 01:20:53
  * @Last Modified By: Roland Breitschaft
- * @Last Modified At: 2018-12-18 01:20:53
+ * @Last Modified At: 2018-12-18 22:39:24
  * @Description: Barrel for the Info Component
  */
 
 export * from './Info';
-
-
-const PROG_VERSION: string = '0.1.0';
-const SCHEMA_VERSION: string = '1.7.1';
-
-/**
- * The current Program Version
- *
- * @returns     {string}    A Program Version
- */
-export function getProductVersion() {
-    return PROG_VERSION;
-}
-
-/**
- * Returns the Schema Version of the AppVersion File
- *
- * @returns {string}    A Schema Version
- */
-export function getSchemaVersion(): string {
-    return SCHEMA_VERSION;
-}
