ef82b0048b6a08216cfdc7308b297ff14c3ea5a5
Roland Breitschaft <roland.breitschaft@x-company.de>
2018-12-19 00:22:57 +0100

    Create only static Methods

--- src/info/Info.ts
+++ src/info/Info.ts
@@ -9,7 +9,7 @@
  * @Email: roland.breitschaft@x-company.de
  * @Create At: 2018-12-18 01:20:07
  * @Last Modified By: Roland Breitschaft
- * @Last Modified At: 2018-12-18 22:04:19
+ * @Last Modified At: 2018-12-18 23:24:47
  * @Description: It's a Helper Class to work with AppVersion Elements
  */
 
@@ -19,12 +19,43 @@ import { Helper } from '../helpers/Helper';
 export class Info {
 
     /**
+     * The current Program Version
+     *
+     * @returns {string}        A Program Version
+     */
+    public static getProductVersion() {
+        let installPath: string | undefined;
+        try {
+            installPath = require.resolve('appversion-mgr');
+        } catch {
+            // Nothing to catch
+        }
+
+        const appVersion = Info.getAppVersionSync(installPath);
+        if (appVersion) {
+            const productVersion = `${appVersion.version.major}.${appVersion.version.minor}.${appVersion.version.patch}`;
+            return productVersion;
+        } else {
+            return Info.PROG_VERSION;
+        }
+    }
+
+    /**
+     * Returns the Schema Version of the AppVersion File
+     *
+     * @returns {string}    A Schema Version
+     */
+    public static getSchemaVersion(): string {
+        return Info.SCHEMA_VERSION;
+    }
+
+    /**
      * Returns the content of appversion.json as a object.
      * Sync version.
      * @param   {string}        directory	The Directory where should looked for the appversion.json
      * @returns	{IAppVersion}   [The AppVersion Object]
      */
-    public getAppVersionSync(directory?: string): IAppVersion | null {
+    public static getAppVersionSync(directory?: string): IAppVersion | null {
         try {
             const helper = new Helper(directory);
             const appVersion = helper.readJson();
@@ -44,7 +75,7 @@ export class Info {
      * @param   {string}                directory	The Directory where should looked for the appversion.json
      * @returns {Promise<IAppVersion>}  A Promise for an AppVersion Object
      */
-    public getAppVersion(directory?: string): Promise<IAppVersion> {
+    public static getAppVersion(directory?: string): Promise<IAppVersion> {
 
         return new Promise<IAppVersion>((resolve, reject) => {
             const helper = new Helper(directory);
@@ -66,15 +97,15 @@ export class Info {
      * @param   {string}    directory	The Directory where should looked for the appversion.json
      * @returns {string}    An AppVersion string, otherwise null.
      */
-    public composePatternSync(pattern: string, directory?: string): string {
+    public static composePatternSync(pattern: string, directory?: string): string {
 
         const splittedPattern = pattern.split('');
-        const appVersion = this.getAppVersionSync(directory);
+        const appVersion = Info.getAppVersionSync(directory);
         let ptt = '';
 
         if (appVersion) {
             splittedPattern.map((ele) => {
-                ptt += this.switchPattern(appVersion, ele);
+                ptt += Info.switchPattern(appVersion, ele);
             });
             return ptt;
         }
@@ -89,22 +120,25 @@ export class Info {
      * @param   {string}    directory	The Directory where should looked for the appversion.json
      * @returns	{Promise<String>} A Promise for an AppVersion string
      */
-    public composePattern(pattern: string, directory?: string): Promise<string> {
+    public static composePattern(pattern: string, directory?: string): Promise<string> {
 
         return new Promise<string>((resolve, reject) => {
             const splittedPattern = pattern.split('');
 
-            this.getAppVersion(directory)
+            Info.getAppVersion(directory)
                 .then((appVersion: IAppVersion) => {
                     let ptt = '';
                     splittedPattern.map((ele) => {
-                        ptt += this.switchPattern(appVersion, ele);
+                        ptt += Info.switchPattern(appVersion, ele);
                     });
                     resolve(ptt);
                 })
                 .catch((err) => console.log(err));
         });
     }
+
+    private static PROG_VERSION: string = '0.1.0';
+    private static SCHEMA_VERSION: string = '1.7.1';
 
     /**
      * Returns the correspondent obj parameter, if not, it returns the given pattern.
@@ -125,7 +159,7 @@ export class Info {
      * . : separator
      * - : separator
      */
-    private switchPattern(appVersion: IAppVersion, pattern: string): string | number | Date | null {
+    private static switchPattern(appVersion: IAppVersion, pattern: string): string | number | Date | null {
         switch (pattern) {
             case 'M' || 'B':
                 return appVersion.version.major;
